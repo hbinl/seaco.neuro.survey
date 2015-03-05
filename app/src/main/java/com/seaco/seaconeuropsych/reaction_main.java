8ec6885264a8a08aeaf9c92921751a8572a2ff10
soomadee Pareatumbee <sspar9@student.monash.edu.au>
2015-03-05 15:17:30 +0800

    xml

--- app/src/main/java/com/seaco/seaconeuropsych/reaction_main.java
+++ app/src/main/java/com/seaco/seaconeuropsych/reaction_main.java
@@ -1,10 +1,13 @@
 package com.seaco.seaconeuropsych;
 
+import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.util.Random;
 
 import android.content.Intent;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
@@ -19,14 +22,31 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 public class reaction_main extends ActionBarActivity {
     int count=0;
     int counter=0;
     long timestart=-1;
     long timefinish;
+    String indexA="";
+    String indexB="";
     double []times=new double[13];
     Button match;
     Button start;
+    double rounded;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -74,7 +94,7 @@ public class reaction_main extends ActionBarActivity {
                         double mean = sum / 10;
                         BigDecimal bd = new BigDecimal(mean);
                         bd = bd.round(new MathContext(3));
-                        double rounded = bd.doubleValue();
+                        rounded = bd.doubleValue();
                         text.setText("Average reaction time is : " + String.valueOf(rounded));
                         match.setVisibility(View.INVISIBLE);
 
@@ -136,75 +156,99 @@ public class reaction_main extends ActionBarActivity {
             case 1 :
             {
                 img1.setImageResource(R.drawable.l1);
+                indexA=indexA+" L1,";
                 img2.setImageResource(R.drawable.l2);
+                indexB=indexB+" L2,";
                 break;
             }
 
             case 2:
             {
                 img1.setImageResource(R.drawable.l3);
+                indexA=indexA+" L3,";
                 img2.setImageResource(R.drawable.l2);
+                indexB=indexB+" L2,";
                 break;
             }
 
             case 3:
             {
                 img1.setImageResource(R.drawable.l3);
+                indexA=indexA+" L3,";
                 img2.setImageResource(R.drawable.l4);
+                indexB=indexB+" L4,";
                 break;
             }
             case 4:
             {
                 img1.setImageResource(R.drawable.l5);
+                indexA=indexA+" L5,";
                 img2.setImageResource(R.drawable.l4);
+                indexB=indexB+" L4,";
                 break;
             }
             case 5:
             {
                 img1.setImageResource(R.drawable.l5);
+                indexA=indexA+" L5,";
                 img2.setImageResource(R.drawable.l6);
+                indexB=indexB+" L6,";
                 break;
             }
             case 6:
             {
                 img1.setImageResource(R.drawable.l6);
+                indexA=indexA+" L6,";
                 img2.setImageResource(R.drawable.l11);
+                indexB=indexB+" L11,";
                 break;
             }
             case 7:
             {
                 img1.setImageResource(R.drawable.l12);
+                indexA=indexA+" L12,";
                 img2.setImageResource(R.drawable.l7);
+                indexB=indexB+" L7,";
                 break;
             }
             case 8:
             {
                 img1.setImageResource(R.drawable.l10);
+                indexA=indexA+" L10,";
                 img2.setImageResource(R.drawable.l8);
+                indexB=indexB+" L8,";
                 break;
             }
             case 9:
             {
                 img1.setImageResource(R.drawable.l9);
+                indexA=indexA+" L9,";
                 img2.setImageResource(R.drawable.l1);
+                indexB=indexB+" L1,";
                 break;
             }
             case 10:
             {
                 img1.setImageResource(R.drawable.l1);
+                indexA=indexA+" L1,";
                 img2.setImageResource(R.drawable.l10);
+                indexB=indexB+" L10,";
                 break;
             }
             case 11:
             {
-                img1.setImageResource(R.drawable.l1);
-                img2.setImageResource(R.drawable.l2);
+                img1.setImageResource(R.drawable.l11);
+                indexA=indexA+" L11,";
+                img2.setImageResource(R.drawable.l8);
+                indexB=indexB+" L8,";
                 break;
             }
             case 12:
             {
                 img1.setImageResource(R.drawable.l12);
+                indexA=indexA+" L12,";
                 img2.setImageResource(R.drawable.l3);
+                indexB=indexB+" L3,";
                 break;
             }
         }
@@ -227,6 +271,55 @@ public class reaction_main extends ActionBarActivity {
                 randompic();
                 //start counter for the number of seconds
                 timestart=System.currentTimeMillis();
+               try{
+                String filepath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + "SEACO" + "/" + "userData.xml";
+                File file= new File(filepath);
+                boolean fileCreated = file.createNewFile();
+
+                DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+                Document doc = docBuilder.parse("file://"+filepath);
+                //Node root=doc.getFirstChild();
+                Node pairlevel1_tag= doc.getElementsByTagName("reaction").item(0);
+                Element root = doc.createElement("reaction");
+                //doc.appendChild(root);
+                Element indA = doc.createElement("indexA");
+                root.appendChild(indA);
+                indA.appendChild(doc.createTextNode(indexA));
+                Element indB = doc.createElement("indexB");
+                root.appendChild(indB);
+                indB.appendChild(doc.createElement(indexB));
+                Element nclick=doc.createElement("NumberofClick");
+                root.appendChild(nclick);
+                nclick.appendChild(doc.createTextNode(String.valueOf(counter)));
+                Element time=doc.createElement("TimeFirst");
+                root.appendChild(time);
+                for (int i=0;i<=12;i++){
+                    time.appendChild(doc.createTextNode(String.valueOf(times[i])));
+                }
+
+                Element meanTime=doc.createElement("mean");
+                root.appendChild(meanTime);
+                meanTime.appendChild(doc.createTextNode(String.valueOf(rounded)));
+                // write the content into xml file
+
+
+                TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                Transformer transformer = transformerFactory.newTransformer();
+                DOMSource source = new DOMSource(doc);
+                StreamResult result = new StreamResult(new File(filepath));
+                StreamResult resultx = new StreamResult(System.out);
+                transformer.transform(source, result);
+                transformer.transform(source, resultx);
+            } catch (ParserConfigurationException pce) {
+                pce.printStackTrace();
+            } catch (TransformerException tfe) {
+                tfe.printStackTrace();
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            } catch (SAXException sae) {
+                sae.printStackTrace();
+            }
 
 
             }
@@ -256,6 +349,8 @@ public class reaction_main extends ActionBarActivity {
             case 1 :
             {
                 img1.setImageResource(R.drawable.l1);
+                indexA=indexA+" L1,";
+                indexB=indexB+" L1,";
                 img2.setImageResource(R.drawable.l1);
                 break;
             }
@@ -264,6 +359,8 @@ public class reaction_main extends ActionBarActivity {
             {
                 img1.setImageResource(R.drawable.l2);
                 img2.setImageResource(R.drawable.l2);
+                indexA=indexA+" L2,";
+                indexB=indexB+" L2,";
                 break;
             }
 
@@ -271,63 +368,85 @@ public class reaction_main extends ActionBarActivity {
             {
                 img1.setImageResource(R.drawable.l3);
                 img2.setImageResource(R.drawable.l3);
+                indexA=indexA+" L3,";
+                indexB=indexB+" L3,";
                 break;
+
             }
             case 4:
             {
                 img1.setImageResource(R.drawable.l4);
                 img2.setImageResource(R.drawable.l4);
+                indexA=indexA+" L4,";
+                indexB=indexB+" L4,";
                 break;
             }
             case 5:
             {
                 img1.setImageResource(R.drawable.l5);
                 img2.setImageResource(R.drawable.l5);
+                indexA=indexA+" L5,";
+                indexB=indexB+" L5,";
                 break;
             }
             case 6:
             {
                 img1.setImageResource(R.drawable.l6);
                 img2.setImageResource(R.drawable.l6);
+                indexA=indexA+" L6,";
+                indexB=indexB+" L6,";
                 break;
             }
             case 7:
             {
                 img1.setImageResource(R.drawable.l7);
                 img2.setImageResource(R.drawable.l7);
+                indexA=indexA+" L7,";
+                indexB=indexB+" L7,";
                 break;
             }
             case 8:
             {
                 img1.setImageResource(R.drawable.l8);
                 img2.setImageResource(R.drawable.l8);
+                indexA=indexA+" L8,";
+                indexB=indexB+" L8,";
                 break;
             }
             case 9:
             {
                 img1.setImageResource(R.drawable.l9);
                 img2.setImageResource(R.drawable.l9);
+                indexA=indexA+" L9,";
+                indexB=indexB+" L9,";
                 break;
             }
             case 10:
             {
                 img1.setImageResource(R.drawable.l10);
                 img2.setImageResource(R.drawable.l10);
+                indexA=indexA+" L10,";
+                indexB=indexB+" L10,";
                 break;
             }
             case 11:
             {
                 img1.setImageResource(R.drawable.l1);
                 img2.setImageResource(R.drawable.l1);
+                indexA=indexA+" L11,";
+                indexB=indexB+" L11,";
                 break;
             }
             case 12:
             {
                 img1.setImageResource(R.drawable.l12);
                 img2.setImageResource(R.drawable.l12);
+                indexA=indexA+" L12,";
+                indexB=indexB+" L12,";
                 break;
             }
         }
+
 
     }
 
